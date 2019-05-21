bdea7be4a02bf4d0f860360733e14b81f95ca27e
Riki Fridrich <riki@fczbkk.com>
2019-05-21 16:41:44 +0200

    feat: add basic functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log('x')
+/**
+ * Check if input is valid email address.
+ * @param {string} input
+ * @returns {boolean}
+ */
+export function isEmail (input) {
+  if (!/\@/.test(input)) {
+    return false
+  }
+
+  if (input.length < 3) {
+    return false
+  }
+
+  return true
+}
+
+/**
+ * Checks if input is valid phone number.
+ * @param {string} input
+ * @returns {boolean}
+ */
+export function isPhoneNumber (input) {
+  if (input.length !== 9) {
+    return false
+  }
+
+  if (/\D/.test(input)) {
+    return false
+  }
+
+  return true
+}
